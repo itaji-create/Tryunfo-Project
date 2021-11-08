74f5df36d218347475a50d554a1bd5f37ebbd322
Itaji Carvalho <itaji.1999@gmail.com>
2021-11-08 17:51:07 -0300

    adiciona desadio 3

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,6 +13,7 @@ class Form extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      hasTrunfo,
       isSaveButtonDisabled,
       onInputChange,
       onSaveButtonClick,
